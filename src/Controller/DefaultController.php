e85e26f6ba8bd6b22623e9634464514f90f343db
WeeMick <michaelblr5@gmail.com>
2021-02-23 16:33:58 +0000

    dashboard route added, twig added via composer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
-    public function number(): Response
+    /**
+     * @Route("/home")
+     */
+    public function home(): Response
     {
         $number = random_int(0, 100);
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
+    }
+
+    /**
+     * @Route("/home/dashboard")
+     */
+    public function dashboard() : Response
+    {
+        $title = "Environmental Data Dashboard";
+
+        return $this->render('dashboard/dashboard.html.twig', [
+            'title' => $title
+        ]);
+
     }
 
 }
